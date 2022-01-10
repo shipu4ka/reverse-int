5b7ae9f217ae3184d9d9e19523b30cb93079bf24
grafit.1 <grafit.1@yandex.ru>
2022-01-10 23:45:12 +0300

    feat: add resolution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str = String(Math.abs(n));
+  let i = 0;
+  let result = '';
+  while (i < str.length) {
+      result = str[i] + result;
+      i = i + 1;    
+  }
+  return Number(result);
 }
